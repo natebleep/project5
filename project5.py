f633abcd1adeff581025d272190fe139d0a5f939
nathan <62918527+natebleep@users.noreply.github.com>
2020-10-13 19:02:07 -0500

    set each function so that it would return json

--- project5.py
+++ project5.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, escape, request, Response
-import random
-import hashlib
-import math
+from hashlib import md5
+import requests
+import json
+from math import factorial
 
 # instansiate the Flask object
 app = Flask(__name__)
@@ -19,47 +20,66 @@ def json_response():
     return resp
 '''
 
-@app.route('/md5/')
-def md5_response( resp = 'Input'):
-    mystring = str(input('Enter a string to hash: '))
-    resp = hashlib.md5(mystring.encode())
-    return (resp.hexdigest())
+@app.route('/md5/<string>')
+def md5_response(string):
+    hash1 = md5(string.encode())
+    hash2 = hash1.hexdigest()
+    return jsonify(
+        input = string,
+        hash = hash2
+    )
 
-@app.route('/factorial/')
-def factorial_response(resp = 'result'):
-    num = int(input('Input a number to compute the factorial: '))
-    resp = math.factorial(num)
-    return f'Output: {resp}'
+@app.route('/factorial/<int:num>')
+def factorial_response(num):
+    if num < 0:
+        fac = "Error: Input must be a positive integer"
+    else:
+        fac = factorial(int(num))
+    return jsonify(
+        input = num,
+        factorial = fac
+    )
 
-@app.route('/fibonacci/')
-def fibonacci_response(resp = 'result'):
-    num = input('Input: ')
-    fib_list = []
-    
-    fib1 = 0
-    fib2 = 1
-    fib3 = 1
+@app.route('/fibonacci/<int:n>')
+def fibonacci_response(n):
+    a = 0
+    b = 1
+    c = 0
+    fib = [a,b]
+    if n <= 0:
+        print("Incorrect input")
+    elif n == 1:
+        return b
+    else:       
+        while c < n:                       
+            c = a + b            
+            a = b
+            if c < n:
+                b = c
+                fib.append(c)
+            else:
+                continue
+        return jsonify(
+            input = n,
+            fibonacci = fib
+        )
 
-    while fib1 <= num:
-        fib_list.append(fib1)
-        fib1 = fib2
-        fib2 = fib3
-        fib3 = fib1 + fib2
-        return f'Output: {fib_list}'
 
-
-@app.route('/is-prime/')
-def isprime_response(resp = 'Input'):
-        num = int(input('Input: '))
+@app.route('/is-prime/<int:num>')
+def isprime_response(num):        
         if num > 1:
             for i in range(2, num):
                 if (num % i) == 0:
-                    return f'{num} is not a prime number'
+                    isprime = False
                     break
             else:
-                return f'{num} is a prime number'
+                isprime = True
         else:
-            return f'{num} is not a prime number'
+            isprime = False
+        return jsonify(
+            input = num,
+            isprime = isprime
+        )
          
 
 @app.route('/slack/<msg>')
